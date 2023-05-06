f06061d45fa46a76b647374f178fff86acdcac4f
Mike <paagrio2005@yandex.ru>
2023-05-06 10:38:01 +0300

    garbage 0.1

--- src/main/java/com/mike/spring/springboot/springboot_rest/security/jwt/JwtConfigurer.java
+++ src/main/java/com/mike/spring/springboot/springboot_rest/security/jwt/JwtConfigurer.java
@@ -4,7 +4,10 @@ package com.mike.spring.springboot.springboot_rest.security.jwt;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private  JwtTokenProvider jwtTokenProvider;
@@ -16,7 +19,7 @@ public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
-        httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore((Filter) jwtTokenFilter, javax.servlet.Filter.class);
 
     }
 
