6e7dbe3521941154f8df9e3342ece42609bcc98b
Mike <paagrio2005@yandex.ru>
2023-06-12 02:26:21 +0300

    run 0.8 80% UserServiceTest

--- src/test/java/com/mike/spring/springboot/springboot_rest/service/UserServiceTest.java
+++ src/test/java/com/mike/spring/springboot/springboot_rest/service/UserServiceTest.java
@@ -14,9 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
@@ -65,6 +67,66 @@ public class UserServiceTest {
             userService.getAll();
         } catch (Exception e) {
             assertTrue(e.getMessage().contains("SQL Exception"));
+        }
+    }
+
+    @Test
+    public void register() {
+
+    }
+
+    @Test
+    public void failedRegister() {
+
+    }
+
+    @Test
+    public void findById() {
+        when(userRepository.findById(isA(Integer.class))).thenReturn(Optional.ofNullable(getActiveUser()));
+        UserEntity user = userService.findById(isA(Integer.class));
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    public void failedFindById() {
+        try {
+            when(userRepository.findById(isA(Integer.class))).thenThrow(new NullPointerException());
+            userService.findById(isA(Integer.class));
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+        }
+    }
+
+    @Test
+    public void findByUsername() {
+        when(userRepository.findByName(isA(String.class))).thenReturn(getActiveUser());
+        UserEntity user = userService.findByUsername("Test");
+        assertNotNull(user.getName());
+    }
+
+    @Test
+    public void failedFindByUsername() {
+        try {
+            when(userRepository.findByName(isA(String.class))).thenThrow(new NullPointerException());
+            userService.findByUsername(isA(String.class));
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+        }
+    }
+
+    @Test
+    public void delete() {
+        userService.delete(isA(Integer.class));
+        Mockito.verify(userRepository, Mockito.times(1)).deleteById(isA(Integer.class));
+    }
+
+    @Test
+    public void failedDelete() {
+        try {
+            Mockito.doThrow(new NullPointerException()).when(userRepository).deleteById(isA(Integer.class));
+            userService.delete(isA(Integer.class));
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
         }
     }
 
